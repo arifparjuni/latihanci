1e7da61419ff132760a060506fdcb80aa39c4754
arifparjuni <arifparjjuni@gmail.com>
2019-09-13 16:09:01 +0700

    menambahkan controller home dan seting jadi default di routs.php

---
+++ application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+class Home extends CI_Controller {
+
+    public function index() {
+        echo "Controller Home";
+    }
+
+    public function hello() {
+        echo "ini adalah function hello di controller Home";
+    }
+
+}
